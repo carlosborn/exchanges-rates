6ef3dfaf6e65c052c3494ed24ce925b441204ccf
carlos@liquidworks.com.br <carlos@liquidworks.com.br>
2024-12-27 16:58:51 -0300

    Created enum CurrencyBase

---
+++ src/main/java/org/exchange/enums/CurrencyBase.java
@@ -0,0 +1,45 @@
+package org.exchange.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum CurrencyBase {
+
+    AUD("AUD", "Australian Dollar"),
+    BGN("BGN", "Bulgarian Lev"),
+    BRL("BRL", "Brazilian Real"),
+    CAD("CAD", "Canadian Dollar"),
+    CHF("CHF", "Swiss Franc"),
+    CNY("CNY", "Chinese Renminbi Yuan"),
+    CZK("CZK", "Czech Koruna"),
+    DKK("DKK", "Danish Krone"),
+    EUR("EUR", "Euro"),
+    GBP("GBP", "British Pound"),
+    HKD("HKD", "Hong Kong Dollar"),
+    HUF("HUF", "Hungarian Forint"),
+    IDR("IDR", "Indonesian Rupiah"),
+    ILS("ILS", "Israeli New Sheqel"),
+    INR("INR", "Indian Rupee"),
+    ISK("ISK", "Icelandic Króna"),
+    JPY("DKK", "Japanese Yen"),
+    KRW("KRW", "South Korean Won"),
+    MXN("MXN", "Mexican Peso"),
+    MYR("MYR", "Malaysian Ringgit"),
+    NOK("NOK", "Norwegian Krone"),
+    NZD("NZD", "New Zealand Dollar"),
+    PHP("PHP", "Philippine Peso"),
+    PLN("PLN", "Polish Złoty"),
+    RON("RON", "Romanian Leu"),
+    SEK("SEK", "Swedish Krona"),
+    SGD("SGD", "Singapore Dollar"),
+    THB("THB", "Thai Baht"),
+    TRY("TRY", "Turkish Lira"),
+    USD("USD", "United States Dollar"),
+    ZAR("ZAR", "South African Rand");
+
+    private final String symbol;
+    private final String description;
+
+}
